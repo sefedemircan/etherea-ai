b4fc620dbf77d25bdc1f8ef6f3b8d405c0c9fcdb
sefedemircan <sefedemircan@gmail.com>
2025-03-23 04:29:46 +0300

    Remove personal assistant description section from PersonalAssistantPage component

--- etherea/src/pages/PersonalAssistantPage.jsx
+++ etherea/src/pages/PersonalAssistantPage.jsx
@@ -13,22 +13,7 @@ function PersonalAssistantPage() {
       <Paper shadow="sm" p="xl" radius="md" bg="etherea.0">
         <PersonalAssistant />
       </Paper>
-      
-      <Paper shadow="sm" p="md" radius="md">
-        <Title order={4} mb="md">Kişisel Asistan Hakkında</Title>
-        <Text c="dimmed" mb="md">
-          Kişisel asistanınız, yapay zeka teknolojisi kullanarak günlük girdilerinizi, duygu durumunuzu ve alışkanlıklarınızı
-          analiz eder. Bu analizler sonucunda size özel içgörüler ve tavsiyeler sunar.
-        </Text>
-        <Text c="dimmed" mb="md">
-          Asistanınız zamanla sizi daha iyi tanır ve önerilerini kişiselleştirir. Ne kadar çok günlük girerseniz,
-          asistanınız o kadar doğru ve faydalı öneriler sunabilir.
-        </Text>
-        <Text c="dimmed">
-          Asistanınızla konuşurken, duygularınızı, düşüncelerinizi ve sorularınızı paylaşabilirsiniz. Asistanınız
-          size empatik ve yapıcı yanıtlar verecektir.
-        </Text>
-      </Paper>
+    
     </Stack>
   );
 }
